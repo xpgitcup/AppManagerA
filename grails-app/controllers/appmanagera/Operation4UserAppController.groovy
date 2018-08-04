aecce503946adef1f18452a59749748d01b45096
LiXiaoping <LiXiaoping@chuyun527>
2018-08-04 17:34:56 +0800

    开始进行实质性设计了...

--- grails-app/controllers/appmanagera/Operation4UserAppController.groovy
+++ grails-app/controllers/appmanagera/Operation4UserAppController.groovy
@@ -4,6 +4,11 @@ class Operation4UserAppController {
 
     def getAppsRunning() {
         def apps = []
+        def ua = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        def ub = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        apps.add(ua)
+        apps.add(ub)
+        println(apps)
         def theModel = [apps: apps]
         if (request.xhr) {
             render(template: "showApps", model:theModel)
@@ -14,6 +19,10 @@ class Operation4UserAppController {
 
     def getAppsInHardDisk() {
         def apps = []
+        def ua = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        def ub = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        apps.add(ua)
+        apps.add(ub)
         def theModel = [apps: apps]
         if (request.xhr) {
             render(template: "showApps", model:theModel)
@@ -24,6 +33,10 @@ class Operation4UserAppController {
 
     def getAppsInDB() {
         def apps = []
+        def ua = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        def ub = new UserApp(appName:'测试程序', appStatus: "????", appTitle: "????")
+        apps.add(ua)
+        apps.add(ub)
         def theModel = [apps: apps]
         if (request.xhr) {
             render(template: "showApps", model:theModel)
--- grails-app/views/operation4UserApp/_showApps.gsp
+++ grails-app/views/operation4UserApp/_showApps.gsp
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>TODO write content</div>
+<head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-    </body>
+<body>
+<div>${apps}</div>
+<table>
+    <thead>
+    <th>应用名</th>
+    <th>id</th>
+    <th>说明</th>
+    <th>状态</th>
+    <th>操作</th>
+    </thead>
+    <tbody>
+    <g:each in="${apps}" var="item" status="i">
+        <tr  class="${(i % 2) == 0 ? 'even' : 'odd'}">
+            <td>${item.appName}</td>
+            <td>${item.appName}</td>
+            <td>${item.appTitle}</td>
+            <td>${item.appStatus}</td>
+            <td></td>
+        </tr>
+    </g:each>
+    </tbody>
+</table>
+</body>
 </html>
