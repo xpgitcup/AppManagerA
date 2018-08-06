835dcb9defe1196a2a0ea610f32472fe8b54b218
LiXiaoping <LiXiaoping@chuyun527>
2018-08-06 17:55:35 +0800

    修正执行流程

--- grails-app/init/appmanagera/BootStrap.groovy
+++ grails-app/init/appmanagera/BootStrap.groovy
@@ -3,7 +3,18 @@ package appmanagera
 class BootStrap {
 
     def init = { servletContext ->
-        
+        def dirName = System.getProperty("user.dir")
+        println("开始搜索${dirName}")
+        def dir = new File(dirName)
+        if (dir.isDirectory()) {
+            def fileList = dir.listFiles()
+            fileList.each { e->
+                println("${e}")
+                println("${e.path}")
+                println("${e.name}")
+                def newApp = new UserApp()
+            }
+        }
     }
     def destroy = {
     }
